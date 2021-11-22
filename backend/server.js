7abf08652f0be025764eb430d3a186f8d870e3d0
Aryan Gupta <76492322+aryangupta1323@users.noreply.github.com>
2021-11-22 17:21:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
 const express=require("express");
 const path=require("path")
-const PORT = process.env.PORT || 3000;
 const cors = require('cors');
+const PORT = process.env.PORT || 3000;
+
 const app=express();
 app.use(express.json())
 
-
+app.use(cors())
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-const corsOptions = {
-  origin: process.env.ALLOWED_CLIENTS.split(',')
-}
-app.use(cors(corsOptions))
+
+
 app.use("/api/files",require("./routers/files"))
 app.use("/files",require("./routers/show"))
 app.use("/files/download",require("./routers/download"))
