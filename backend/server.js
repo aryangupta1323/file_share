38e1371243e15d8815be05cbafd06b935a3d4bea
Aryan Gupta <76492322+aryangupta1323@users.noreply.github.com>
2021-11-22 21:01:23 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app=express();
 app.use(express.json())
 
 app.use(cors())
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
