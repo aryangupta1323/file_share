77c5512cbbf0ac298caa34746990180a649dc0d7
Aryan Gupta <76492322+aryangupta1323@users.noreply.github.com>
2021-11-22 14:29:15 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,19 @@
 const express=require("express");
 const path=require("path")
-const bodyParser=require("body-parser")
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 const app=express();
 app.use(express.json())
-app.use(bodyParser.urlencoded({extended:true}))
+
+
 app.use(express.static('public'))
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions))
 app.use("/api/files",require("./routers/files"))
 app.use("/files",require("./routers/show"))
 app.use("/files/download",require("./routers/download"))
