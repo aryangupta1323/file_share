8268d5c6d8a9d1e455b9979dfa0efd60ee5e595e
Aryan Gupta <76492322+aryangupta1323@users.noreply.github.com>
2021-11-27 17:06:04 +0530

    Add files via upload

---
+++ backend/public/javascript/script.js
@@ -0,0 +1,24 @@
+console.log(toggle);
+toggle.addEventListener("change",changetheme);
+function changetheme(event){
+  console.log(event.target.checked);
+  if(event.target.checked)
+  {document.documentElement.setAttribute("data-theme","dark");
+    darkmode();
+    localStorage.setItem("theme","dark")
+  }
+  else
+  {document.documentElement.setAttribute("data-theme","light");
+  lightmode();
+  localStorage.setItem("theme","light");
+}
+
+}
+function darkmode(){
+  document.querySelector("#toggle-icon .toggle-text").textContent="Dark Mode";
+  document.querySelector("#toggle-icon i").classList.replace("fa-sun","fa-moon");
+}
+function lightmode(){
+  document.querySelector("#toggle-icon .toggle-text").textContent="Light Mode";
+  document.querySelector("#toggle-icon i").classList.replace("fa-moon","fa-sun");
+}
